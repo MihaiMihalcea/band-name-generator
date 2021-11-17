7495a8f46911b746d1acf4336ee238fcf5d74943
Mihai Mihalcea <contact@mihaimihalcea.name>
2021-11-17 22:23:58 +0200

    Added version 1.0 of the Band Name Generator project

---
+++ band name generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator!")
+
+city = input("\nWhat is the city you grew up in? ")
+pet_name = input(" \nWhat is your pet's name? ")
+
+print(f"\nYour band name could be: {city} {pet_name}")
